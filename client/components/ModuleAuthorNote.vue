647fcdf3ecb68416b068227d21d3fda6a2e842f8
LouisMazel <me@loicmazuel.com>
2023-05-22 18:44:21 +0200

    chore: format code

--- client/components/ModuleAuthorNote.vue
+++ client/components/ModuleAuthorNote.vue
@@ -1,12 +1,6 @@
 <template>
-  <NTip
-    n="teal"
-    class="py-4"
-  >
-    Documentation available in the <NLink
-      href="https://github.com/zadigetvoltaire/nuxt-gtm#readme"
-      target="_blank"
-    >
+  <NTip n="teal" class="py-4">
+    Documentation available in the <NLink href="https://github.com/zadigetvoltaire/nuxt-gtm#readme" target="_blank">
       README.me
     </NLink> on our github project.
   </NTip>
